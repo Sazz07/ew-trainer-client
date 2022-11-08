87f29d80f0e2dff725710be8b9e26b2936be8dc0
Sazz07 <sazzad.hossain882@gmail.com>
2022-11-09 02:39:25 +0600

    Firebase setup complete with environment variable

--- src/contexts/AuthProvider/AuthProvider.js
+++ src/contexts/AuthProvider/AuthProvider.js
@@ -1,13 +1,19 @@
 import React, { createContext, useState } from 'react';
+import app from '../../firebase/firebase.config';
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
 
 export const AuthContext = createContext();
-
+const auth = getAuth(app)
 
 const AuthProvider = ({ children }) => {
     const [user, setUser] = useState(null);
 
 
+    // create user with email and password
 
+    const createUser = (email, password) => {
+        return createUserWithEmailAndPassword(auth, email, password);
+    }
 
 
     const authInfo = {
